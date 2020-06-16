757eb751265db02bd90ecc7a5157f4eb3af1debd
cp2004 <31997505+cp2004@users.noreply.github.com>
2020-06-16 21:46:39 +0100

    Install jq incase it is not installed

--- upgrade_to_py3.sh
+++ upgrade_to_py3.sh
@@ -1,5 +1,9 @@
 #! /bin/bash
 echo "This script is about to perform an upgrade of your OctoPrint install from python 2 to 3"
+echo "It will install jq to help manage the json files"
+echo "WORK IN PROGRESS"
+echo "Press [enter] to continue or ctrl-c to quit"
+read
 
 #Test if we are running OctoPi, else need path to env
 OCTOPI_VERSION_FILE=/etc/octopi_version
@@ -28,5 +32,28 @@ PATH_TO_OCTOPRINT=$PATH_TO_VENV/bin/octoprint
 #Try to create a backup
 echo "Creating a backup so we can read the plugin list"
 
-BACKUP_NAME=$($PATH_TO_VENV/bin/octoprint plugins backup:backup --exclude timelapse --exclude uploads | grep -oP '(?<=Creating backup at )(.*)(?=,)')
-echo $BACKUP_NAME
+BACKUP_NAME=$($PATH_TO_VENV/bin/octoprint plugins backup:backup --exclude timelapse --exclude uploads | grep -oP '(?<=Creating backup at )(.*)(?=.zip)')
+PATH_TO_BACKUP=/home/$(whoami)/.octoprint/data/backup/$BACKUP_NAME.zip
+echo "Backup created at " $PATH_TO_BACKUP
+
+#Now we need to find a list of plugin urls from the backup/plugin-list.json
+#And match them to the repo
+
+BACKUP_TARGET=/home/$(whoami)/.octoprint/data/backup/$BACKUP_NAME
+output=$(unzip $PATH_TO_BACKUP -d $BACKUP_TARGET)
+rm $PATH_TO_BACKUP
+echo "Removing zip now we've extracted it"
+if test -f "$BACKUP_TARGET/plugin_list.json"; then
+    PLUGINS_INSTALLED=true
+    echo "Detected plugins installed"
+else
+    PLUGINS_INSTALLED=false
+    echo "No plugins found, if this is not correct please ask for help"
+    echo "(Note this does not include bundled plugins)"
+fi
+
+echo "Installing jq to manage plugin-list.json... (If not already)"
+sudo apt-get install jq -y
+
+echo "Removing backup"
+rm -rf $BACKUP_TARGET
