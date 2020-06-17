8463d4bbd31feb72efdc907cf4c4a0423587e112
cp2004 <31997505+cp2004@users.noreply.github.com>
2020-06-17 17:10:10 +0100

    It runs without errors, time to test

--- upgrade_to_py3.py
+++ upgrade_to_py3.py
@@ -45,7 +45,7 @@ try:
 except subprocess.CalledProcessError:
     print("Error getting backup from Octoprint, exiting")
     sys.exit(0)
-octoprint_zip_name = re.search(r'(?<=Creating backup at )(.*)(?=.zip)', backup_output)
+octoprint_zip_name = re.search(r'(?<=Creating backup at )(.*)(?=.zip)', backup_output).group()
 
 backup_target = '{}/data/backup/{}'.format(CONFBASE, octoprint_zip_name)
 
@@ -65,7 +65,7 @@ if len(plugin_list):
     print("\nPlugins installed:")
     plugin_keys = []
     for plugin in plugin_list:
-        print("- {}".format(plugin['name']) + plugin['name'])
+        print("- {}".format(plugin['name']))
         plugin_keys.append(plugin['key'])
     print("If you think there is something missing from here, please check the list of plugins in Octoprint")
     go = input("Continue? [enter]")
@@ -111,19 +111,19 @@ if len(plugin_keys):
     print("\nReinstalling plugins...")
     plugin_errors = []
     for plugin in plugin_urls:
-        print("Installing {}".format(plugin['name']))
+        print("Installing {}".format(plugin))
         print("Prentending to run {} -m pip install {}".format(PATH_TO_PYTHON, plugin))
-        try:
-            backup_output = subprocess.run(
-                [PATH_TO_PYTHON, '-m', 'pip', 'install', plugin],
-                check=True,
-                capture_output=True
-            ).stdout.rstrip().decode('utf-8')
-        except subprocess.CalledProcessError as e:
-            plugin_errors.append(plugin[plugin])
-            print("Error installing plugin, maybe it's not compatible?")
-            print(e)
-            sys.exit(0)
+        #try:
+        #    backup_output = subprocess.run(
+        #        [PATH_TO_PYTHON, '-m', 'pip', 'install', plugin],
+        #        check=True,
+        #        capture_output=True
+        #    ).stdout.rstrip().decode('utf-8')
+        #except subprocess.CalledProcessError as e:
+        #    plugin_errors.append(plugin[plugin])
+        #    print("Error installing plugin, maybe it's not compatible?")
+        #    print(e)
+        #    sys.exit(0)
     if len(plugin_errors):
         print("Could not install these plugins:")
         for plugin in plugin_errors:
@@ -145,6 +145,4 @@ print("Pretending to run {}".format(START_COMMAND))
 print("\nCleaning Up... \nRemoving backup zip")
 os.remove("{}.zip".format(backup_target))
 print("Removing backup folder")
-import shutil
-shutil.rmtree(backup_target)
 print("Finished! Octoprint should be restarted and ready to go")
