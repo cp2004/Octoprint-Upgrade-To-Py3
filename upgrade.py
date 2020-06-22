27a863b4cdf74a1b3f1a03246cc753a5b6acaa3f
cp2004 <31997505+cp2004@users.noreply.github.com>
2020-06-22 12:37:48 +0100

    Add colored output! Subject to testing

--- upgrade.py
+++ upgrade.py
@@ -12,6 +12,20 @@ import zipfile
 import requests
 import re
 
+BASE = '\033['
+
+
+class TextColors:
+    RESET = BASE + '39m'
+    RED = BASE + '31m'
+    GREEN = BASE + '32m'
+    YELLOW = BASE + '33m'
+
+
+class TextStyles:
+    BRIGHT = BASE + '1m'
+    NORMAL = BASE + '22m'
+
 
 def oprint_version_gt_141(venv_path):
     try:
@@ -21,38 +35,40 @@ def oprint_version_gt_141(venv_path):
             capture_output=True
         ).stdout.rstrip().decode('utf-8')
     except subprocess.CalledProcessError:
-        print("Failed to find OctoPrint install")
+        print("{}Failed to find OctoPrint install{}".format(TextColors.RED, TextColors.RESET))
         print("If this is not OctoPi, please check that you have specified the right virtual env")
         sys.exit(0)
 
     version_no = re.search(r"(?<=version )(.*)", output).group().split('.')
-    print("Octoprint version: {}.{}.{}".format(version_no[0], version_no[1], version_no[2]))
+    print("OctoPrint version: {}.{}.{}".format(version_no[0], version_no[1], version_no[2]))
     if int(version_no[0]) >= 1 and int(version_no[1]) >= 4:
         if int(version_no[2]) > 0:
             return True
         else:
             return False
     else:
-        # This is not strictly needed, but since I am only testing this against Octoprint 1.4.0 or later
+        # This is not strictly needed, but since I am only testing this against OctoPrint 1.4.0 or later
         # I cannot guarantee behaviour of previous versions
-        print("Please upgrade to an OctoPrint version >= 1.4.0 for Python 3 compatibility")
+        print("{}Please upgrade to an OctoPrint version >= 1.4.0 for Python 3 compatibility{}".format(TextColors.YELLOW, TextColors.RESET))
         sys.exit(0)
 
 
 # Intro text
-print("This script is about to perform an upgrade of your OctoPrint install from python 2 to 3")
-print("It requires an internet connection to run")
-print("**This action will disrupt any ongoing print jobs**")
+print("This script is about to perform an upgrade of your OctoPrint install from Python 2 to Python 3")
+print("{}It requires an internet connection to run".format(TextColors.YELLOW))
+print("{}{}This action will disrupt any ongoing print jobs{}{}".format(
+    TextColors.RED, TextStyles.BRIGHT, TextColors.RESET, TextStyles.NORMAL))
 print("It will install the latest OctoPrint release (1.4.0), as well as the latest version of all plugins")
 print("No configuration or other files will be overwritten")
 print("If the install fails, download the 'go_back.py' file here: https://github.com/cp2004/Octoprint-Upgrade-To-Py3/go_back.py")
-confirm = input("Press [enter] to continue or ctrl-c to quit")
+confirm = input("Press {}[enter]{} to continue or ctrl-c to quit".format(TextColors.GREEN, TextColors.RESET))
+
 
 # Detect OctoPi or prompt for paths
 PATH_TO_VENV = None
 CONFBASE = None
 if os.path.isfile("/etc/octopi_version"):
-    print("\nDetected OctoPi installation")
+    print("\n{}Detected OctoPi installation{}".format(TextColors.GREEN, TextColors.RESET))
     PATH_TO_VENV = "/home/pi/oprint"
     STOP_COMMAND = "sudo service octoprint stop"
     START_COMMAND = "sudo service octoprint start"
@@ -61,7 +77,7 @@ if os.path.isfile("/etc/octopi_version"):
     if not OPRINT_GT_141:
         CONFBASE = "/home/pi/.octoprint"
 else:
-    print("\nManual install detected")
+    print("\n{}Detected manual installation{}".format(TextColors.GREEN, TextColors.RED))
     print("Please provide the path to your virtual environment and the config directory of octoprint")
     while not PATH_TO_VENV:
         path = input("Path: ")
@@ -76,13 +92,14 @@ else:
         while not CONFBASE:
             CONFBASE = input("Config directory: ")
             if os.path.isfile(os.path.join(CONFBASE, 'config.yaml')):
-                print("Config directory valid")
+                print("{}Config directory valid{}".format(TextColors.GREEN, TextColors.RESET))
             else:
-                print("Invalid path, please try again")
+                print("{}Invalid path, please try again{}".format(TextColors.GREEN, TextColors.RESET))
                 CONFBASE = None
     print("\nTo do the install, we need the service stop and start commands.")
     STOP_COMMAND = input("Stop command: ")
     START_COMMAND = input("Start command: ")
+
 
 # Create backup to read the plugin list
 print("\nCreating a backup so we can read the plugin list")
@@ -93,7 +110,7 @@ try:
         capture_output=True
     ).stdout.rstrip().decode('utf-8')
 except subprocess.CalledProcessError:
-    print("Error getting backup from Octoprint")
+    print("{}Error getting backup from OctoPrint{}".format(TextColors.RED, TextColors.GREEN))
     print("If you are on a manual install, please check octoprint is installed in the venv specified")
     sys.exit(0)
 
@@ -103,8 +120,8 @@ else:
     octoprint_zip_name = re.search(r'(?<=Creating backup at )(.*)(?=.zip)', backup_output).group()
     backup_target = '{}/data/backup/{}'.format(CONFBASE, octoprint_zip_name)
 
+
 # Extract plugin_list.json from the backup
-print("Extracting plugin_list.json from backup")
 with zipfile.ZipFile('{}.zip'.format(backup_target), 'r') as zip_ref:
     try:
         zip_ref.getinfo("plugin_list.json")
@@ -116,6 +133,7 @@ with zipfile.ZipFile('{}.zip'.format(backup_target), 'r') as zip_ref:
         with zip_ref.open("plugin_list.json") as plugins:
             plugin_list = json.load(plugins)
 
+
 # Generate a list of installed plugin keys
 if plugin_list:
     print("\nPlugins installed:")
@@ -124,12 +142,13 @@ if plugin_list:
         print("- {}".format(plugin['name']))
         plugin_keys.append(plugin['key'])
     print("If you think there is something missing from here, please check the list of plugins in Octoprint")
-    go = input("Continue? [enter]")
+    go = input("Continue? {}[enter]{}".format(TextColors.GREEN, TextColors.RESET))
 else:
     plugin_keys = []
     print("No plugins found")
     print("If you think this is an error, please ask for help. Note this doesn't include bundled plugins.")
-    go = input("Press [enter] to continue, or ctrl-c to quit")
+    go = input("Press {}[enter]{} to continue, or ctrl-c to quit".format(TextColors.GREEN, TextColors.RESET))
+
 
 # Move octoprint venv, create new one, install octoprint
 PATH_TO_PYTHON = '{}/bin/python'.format(PATH_TO_VENV)  # Note this is the VIRTUALENV python
@@ -139,7 +158,7 @@ commands = [
     ['virtualenv', '--python=/usr/bin/python3', PATH_TO_VENV],  # Only time we want to use system python
     [PATH_TO_PYTHON, '-m', 'pip', 'install', 'OctoPrint']
 ]
-print("\nMoving venv and installing octoprint... (This may take a while - Do not cancel!)")
+print("\nCreating Python 3 virtual environment and installing OctoPrint... (This may take a while - Do not cancel!)")
 for command in commands:
     try:
         output = subprocess.run(
@@ -148,14 +167,14 @@ for command in commands:
             capture_output=True
         ).stdout.decode('utf-8')
     except subprocess.CalledProcessError as e:
-        print("ERROR: Failed to install Octoprint")
+        print("{}ERROR: Failed to install Octoprint{}".format(TextColors.RED, TextColors.RESET))
         print(e)
         # Remove zip
         print("\nCleaning Up... \nRemoving backup zip")
         os.remove("{}.zip".format(backup_target))
         print("Exiting")
         sys.exit(0)
-print("Octoprint successfully installed")
+print("{}Octoprint successfully installed{}".format(TextColors.GREEN, TextColors.RESET))
 
 # Create list of plugin urls, then install one by one
 if len(plugin_keys):
@@ -181,25 +200,26 @@ if len(plugin_keys):
             ).stdout.rstrip().decode('utf-8')
         except subprocess.CalledProcessError as e:
             plugin_errors.append(plugin[plugin])
-            print("Error installing plugin, maybe it's not compatible?")
+            print("{}Error installing plugin{}".format(TextColors.RED, TextColors.RESET))
             print(e)
             sys.exit(0)
     if len(plugin_errors):
-        print("Could not install these plugins:")
+        print("{}Could not install these plugins:{}".format(TextColors.YELLOW, TextColors.RESET))
         for plugin in plugin_errors:
             print(" - {}".format(plugin))
         print("Reasons for this could be: \n- Not on the repository (Installed from uploaded archive/url) \n- Incompatible with your system")
-        print("It is recommended that you reinstall them when you log back into octoprint")
+        print("It is recommended that you reinstall them when you log back into OctoPrint")
 
     # Print plugins that were not on the repo
-    print("\nThese plugins were not found on the repo, please install them manually")
+    print("\n{}These plugins were not found on the repo{}".format(TextColors.YELLOW, TextColors.RESET))
+    print("Please install them manually, from OctoPrint's Plugin manager")
     for not_found_plugin in plugin_keys:
         for plugin in plugin_list:
             if plugin['key'] == not_found_plugin:
-                print("- {}".format(plugin['name']))
+                print("- {}, ".format(plugin['name']))
 
 # Restart OctoPrint, and clean up
-print("\nStarting Octoprint")
+print("\nStarting OctoPrint")
 try:
     cmd_output = subprocess.run(
         START_COMMAND.split(),
@@ -207,11 +227,11 @@ try:
         capture_output=True
     ).stdout.rstrip().decode('utf-8')
 except subprocess.CalledProcessError as e:
-    print("Error starting the OctoPrint service")
+    print("{}Error starting the OctoPrint service{}".format(TextColors.RED, TextColors.RESET))
     print(e)
     sys.exit(0)
 
 print("\nCleaning Up... \nRemoving backup zip")
 os.remove("{}.zip".format(backup_target))
-print("\nFinished! Octoprint should be restarted and ready to go")
+print("\n{}Finished! Octoprint should be restarted and ready to go{}".format(TextColors.GREEN, TextColors.RESET))
 print("Once you have verified the install works, you can safely remove the folder {}.bak".format(PATH_TO_VENV))
