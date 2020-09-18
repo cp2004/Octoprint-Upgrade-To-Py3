cp2004/Octoprint-Upgrade-To-Py3

commit c8afa608f4d549a51c26d916b873d5c02dce127b

:bookmark: Version to 2.1.5

upgrade.py
@@ -32,7 +32,7 @@ import time
 import argparse
 
 # CONSTANTS
-SCRIPT_VERSION = '2.1.4'
+SCRIPT_VERSION = '2.1.5'
 LATEST_OCTOPRINT = '1.4.2'
 
 BASE = '\033['

commit d5de586cf71f35b1a98cfed12e84c3f04b0ea3c6

:bug: Fix python '+' stripping introduced by c8afa60

upgrade.py
@@ -341,8 +341,8 @@ def check_venv_python(venv_path):
     version_str, poll = get_python_version(venv_path)
     for line in version_str:
         # Debian has the python version set to 2.7.15+ which is not PEP440 compliant (bug 914072)
-        if version_str.endswith("+"):
-            version_str = version_str[:-1]
+        if line.endswith("+"):
+            line = line[:-1]
 
         match = re.search(r"^Python (?P<major>0|[1-9]\d*)\.(?P<minor>0|[1-9]\d*)\.(?P<patch>0|[1-9]\d*)(?:-(?P<prerelease>(?:0|[1-9]\d*|\d*[a-zA-Z-][0-9a-zA-Z-]*)(?:\.(?:0|[1-9]\d*|\d*[a-zA-Z-][0-9a-zA-Z-]*))*))?(?:\+(?P<buildmetadata>[0-9a-zA-Z-]+(?:\.[0-9a-zA-Z-]+)*))?$", line.rstrip())
         if match: